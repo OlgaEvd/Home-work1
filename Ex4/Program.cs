af7788da20c55e057d0bfac80624a2e91e85a9a7
«Olga» <elfica@yandex.ru>
2022-06-24 02:35:09 +0800

    Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел

---
+++ Ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел.");
+Console.WriteLine(    );
+Console.WriteLine("Введите три числа");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max)max=b;
+if (c>max)max=c;
+
+Console.WriteLine($"{max} максимальное число:", max);
