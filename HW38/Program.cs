2bae02a00d33449f8bdd4b9a8393489be963c362
«Olga» <elfica@yandex.ru>
2022-07-14 23:38:06 +0800

    задача 38

---
+++ HW38/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+
+double[] array = {3.5, 7.1, 22.9, 2.3, 78.5};
+double max = array[0];
+double min = array[0];
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max) max = array[i];
+    if (array[i] < min) min = array[i];
+}
+
+Console.WriteLine(max - min);
