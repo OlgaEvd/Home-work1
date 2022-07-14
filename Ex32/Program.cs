027872153c9a968c8cb641509c6a2ab7c66594c1
«Olga» <elfica@yandex.ru>
2022-07-14 17:09:02 +0800

    задача 32

---
+++ Ex32/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 32: Напишите программу замена элементов
+//массива: положительные элементы замените на
+//соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+int[] CreateArray(int size)
+{
+int[] array = new int [size];
+var rnd = new Random();
+int index = 0;
+
+while (index < array.Length)
+    {
+        array[index] = rnd.Next(-100,100);
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+    return array;
+}
+
+  int[] Change(int[] arr)
+{
+    int[] newArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        newArr[i] = -arr[i];
+        Console.Write($"{newArr[i]} ");
+        }
+    return newArr;
+}
+int [] array = CreateArray(4);
+Console.WriteLine();
+int[] array1 = Change(array);
+
