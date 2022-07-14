ba2349bd71d6e7e04fca61ea0b0028d5912bf2ef
«Olga» <elfica@yandex.ru>
2022-07-14 22:27:00 +0800

    задача 34

---
+++ HW34/Program.cs
@@ -0,0 +1,39 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//Пример
+//[345, 897, 568, 234] -> 2
+
+int count = 0;
+int[] CreateArray(int size)
+{
+int[] array = new int [size];
+var rnd = new Random();
+int index = 0;
+
+while (index < array.Length)
+    {
+        array[index] = rnd.Next(100,999);
+        Console.Write($"{array[index]} ");
+        index++;
+    }
+    return array;
+}
+
+int sumPolElement(int [] arr)
+{
+
+   for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] % 2 == 0) 
+    count = count + 1;
+}
+ return arr [count];
+}
+
+
+
+
+int [] array = CreateArray(4);
+Console.WriteLine();
+int sum = sumPolElement(array);
+Console.Write(count);
